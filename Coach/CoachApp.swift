1f34ef15cce061e2d37535447e1ba6860d884d07
Bryan de Bourbon <bryandebourbon@gmail.com>
2024-04-06 13:05:52 -0400

    Initial Commit

---
+++ Coach/CoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachApp.swift
+//  Coach
+//
+//  Created by Bryan de Bourbon on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
